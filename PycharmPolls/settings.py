f53406c8ece4b16a8b52d321d7c65c226363749c
mad.crokodile <mad.crokodile@gmail.com>
2015-01-24 21:12:54 +0300

    add project diff to repo

--- PycharmPolls/settings.py
+++ PycharmPolls/settings.py
@@ -56,12 +56,25 @@ WSGI_APPLICATION = 'PycharmPolls.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djbook_polls',
+        'USER': 'djbook',
+        'HOST': '127.0.0.1',
     }
 }
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
